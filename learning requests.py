aea8bacf59846131840c7c86c471754fd1ca66e8
Abilash <abilash2574@gmail.com>
2020-05-07 21:44:56 +0530

    This is the start

---
+++ learning requests.py
@@ -0,0 +1,11 @@
+import requests
+url = "https://i.picsum.photos/id/422/1200/500.jpg"
+
+
+print("1",'Next')
+print("2",'Previous')
+print("3",'Random')
+print("4",'Exit')
+
+list = range(700)
+li = iter(list)
